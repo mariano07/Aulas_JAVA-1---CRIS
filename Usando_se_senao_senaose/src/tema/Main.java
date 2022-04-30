dc070f89148f0febe7d1ec7e6eefafbc4cc86f37
Hymdell <cristopherpiussi@gmail.com>
2022-04-30 00:43:35 -0300

    Atividade_IF_ELSE_IFELSE

---
+++ Usando_se_senao_senaose/src/tema/Main.java
@@ -0,0 +1,32 @@
+package tema;
+
+import javax.swing.JOptionPane;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String nome,cep,cpf,confirmacao;
+		
+		JOptionPane.showInternalMessageDialog(null, "Bem vindo a mini Agenda");
+		nome = JOptionPane.showInputDialog("Informe seu nome");
+		cep = JOptionPane.showInputDialog("Informe seu CEP");
+		cpf = JOptionPane.showInputDialog("Informe seu CPF");
+		JOptionPane.showMessageDialog(null, "Confira seus dados" + "\nNome: " + nome + "\nCEP: " + cep + "\nCPF: " + cpf);
+		JOptionPane.showInternalMessageDialog(null, "Seus dados estão corretos?");
+		confirmacao = JOptionPane.showInputDialog("Digite Sim,Não ou Sair");
+		
+		if(confirmacao.equals("Sim")) {
+			JOptionPane.showInternalMessageDialog(null, "Obrigado por usar a mini Agenda");
+		}else if (confirmacao.equals("Não")) {
+			JOptionPane.showInternalMessageDialog(null, "Informe seus dados novamente por favor");
+			nome = JOptionPane.showInputDialog("Informe seu nome");
+			cep = JOptionPane.showInputDialog("Informe seu CEP");
+			cpf = JOptionPane.showInputDialog("Informe seu CPF");
+			JOptionPane.showMessageDialog(null, "Confira seus dados" + "\nNome: " + nome + "\nCEP: " + cep + "\nCPF: " + cpf);
+		}else {
+			JOptionPane.showInternalMessageDialog(null, "Tchau :)");
+		}
+	}
+
+}
