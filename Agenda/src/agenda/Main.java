a08c5959c75606e95983de8931a7a0cc9f21af14
Hymdell <cristopherpiussi@gmail.com>
2022-05-04 00:19:51 -0300

    Update Main.java

--- Agenda/src/agenda/Main.java
+++ Agenda/src/agenda/Main.java
@@ -15,6 +15,7 @@ public class Main {
 			option = Integer.parseInt(JOptionPane.showInputDialog("Informe qual opção deseja realizar\n[1]Adicionar Dados\n[2]Listar Dados Existentes\n[3]Mudar Dados\n[4]Sair"));
 			//Menu options
 			switch(option) {
+				//Adding data
 				case 1: {
 					name = JOptionPane.showInputDialog("Informe o Nome que deseja cadastrar");
 					cep = JOptionPane.showInputDialog("Informe o CEP que deseja cadastrar");
@@ -22,10 +23,12 @@ public class Main {
 					cel_number = JOptionPane.showInputDialog("Informe o Telefone que deseja cadastrar");
 					break;
 				}
+				//Listing data
 				case 2: {
 					JOptionPane.showMessageDialog(null, "Os dados registrados são:\nNome: " + name + "\nCEP: " + cep + "\nEndereço: " + adress + "\nTelefone: " + cel_number);
 					break;
 				}
+				//Chaging data
 				case 3: {
 					changeOption = Integer.parseInt(JOptionPane.showInputDialog("Informe qual dado deseja altera\n[1]Nome\n[2]CEP\n[3]Endereço\n[4]Telefone\n[5]Sair"));
 					switch (changeOption) {
@@ -51,6 +54,7 @@ public class Main {
 					}
 					break;
 				}
+				//exit program
 				default: {
 					i = 4;
 					break;
