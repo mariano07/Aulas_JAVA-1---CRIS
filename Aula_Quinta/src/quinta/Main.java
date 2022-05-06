cb5aff21ddce3c1c56d8d2b8bc0c37017774eebc
Cristopher Machado <cristopherpiussi@gmail.com>
2022-05-05 21:09:50 -0300

    Exercicio_matriz

---
+++ Aula_Quinta/src/quinta/Main.java
@@ -0,0 +1,30 @@
+package quinta;
+
+public class Main {
+	public static int[][] TesteMatriz(int l, int c){
+		int [][] matriz = new int [l][c];
+		int i=0,j=0;
+		
+		for(i=0;i<l;i++) {
+			for(j=0;j<c;j++) {
+				if(i % 2 == 0) {
+					matriz[i][j] = 0;
+				}else {
+					matriz[i][j] = 1;
+				}
+			}
+		}
+		for(int [] linha: matriz) {
+			System.out.print("\n");
+			for(int elemento : linha) {
+				System.out.print(" " + elemento);
+			}
+		}
+		return matriz;
+	}
+	public static void main(String[] args) {
+		TesteMatriz(10,3);
+
+	}
+
+}
