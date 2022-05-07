38703c1bfd80a5a6e3a05298d3d76a75fe50d18d
Hymdell <cristopherpiussi@gmail.com>
2022-05-06 22:32:02 -0300

    Update Register.java

--- Escola_Exercicio/src/escola/Register.java
+++ Escola_Exercicio/src/escola/Register.java
@@ -1,6 +1,6 @@
 package escola;
 
-import java.util.Arrays;
+
 
 import javax.swing.JOptionPane;
 
@@ -51,28 +51,30 @@ public class Register {
 	//Ask if you want to register new students
 	public void Repeat(int l,int c,String[][] students) {
 		
-		op = Integer.parseInt(JOptionPane.showInputDialog("Escolha\n[1]Registrar mais alunos\n[2]Ver alunos registrados\n[3]Sair"));
-		if(op == 1) {
-			StudentsNames2(l,c,students);
-		}else if(op == 2){
-			//Creating SringBuild
-			StringBuilder sb = new StringBuilder(l);
-			sb.append("<html><table>");
-			for(x=0;x<l;x++) {
-				sb.append("<tr><td>");
-				sb.append("[Aluno]");
-				sb.append("</td>");
-				for(y=0;y<c;y++) {
-					sb.append("<td>");
-					sb.append("[" + students[x][y] + "]");
+		while(op != 3) {
+			op = Integer.parseInt(JOptionPane.showInputDialog("Escolha\n[1]Registrar mais alunos\n[2]Ver alunos registrados\n[3]Sair"));
+			if(op == 1) {
+				StudentsNames2(l,c,students);
+			}else if(op == 2){
+				//Creating SringBuild
+				StringBuilder sb = new StringBuilder(l);
+				sb.append("<html><table>");
+				for(x=0;x<l;x++) {
+					sb.append("<tr><td>");
+					sb.append("[Aluno]");
 					sb.append("</td>");
+					for(y=0;y<c;y++) {
+						sb.append("<td>");
+						sb.append("[" + students[x][y] + "]");
+						sb.append("</td>");
+					}
+					sb.append("</tr");
 				}
-				sb.append("</tr");
+				sb.append("</table></html>");
+				JOptionPane.showMessageDialog(null, sb.toString());
+			}else {
+				JOptionPane.showMessageDialog(null,"Obrigado por usar nosso software");
 			}
-			sb.append("</table></html>");
-			JOptionPane.showMessageDialog(null, sb.toString());
-		}else {
-			JOptionPane.showMessageDialog(null,"Obrigado por usar nosso software");
 		}
 	}
 	//Registering new students
@@ -81,12 +83,16 @@ public class Register {
 		int l1=0;
 		double average=0.0;
 		//Getting Array size
-		l1 = Integer.parseInt(JOptionPane.showInputDialog("Informe o tamanho da matriz"));
+		l1 = Integer.parseInt(JOptionPane.showInputDialog("Informe o novo número de alunos"));
 		//Creating first Array
-		String[][] students2 = new String[l][c];
+		String[][] students2 = new String[l1][c];
 		//Passing old data to new array
-		for(x=0;i<l;i++) {
-			students2[i] = students[i];
+		i=1;
+		for(x=0;x<l;x++) {
+			i++;
+			for(y=0;y<c;y++) {
+				students2[x][y] = students[x][y];
+			}
 		}
 		//Registering students names
 		for(x=l;x<l1;x++) {
@@ -95,26 +101,26 @@ public class Register {
 		//Resetting variable for counting
 		i=1;
 		//Getting Grades
-		for(x=0;x<l;x++) {
+		for(x=l;x<l1;x++) {
 			i=1;
 			for(y=1;y<4;y++) {
-				students[x][y] = JOptionPane.showInputDialog("Informe a " + i++ + "º nota " + "do aluno " + students[x][0]);
+				students2[x][y] = JOptionPane.showInputDialog("Informe a " + i++ + "º nota " + "do aluno " + students2[x][0]);
 			}
 		}
 		//Making each student average grade
 		for(x=l;x<l1;x++) {
 			average=0;
 			for(y=1;y<4;y++) {
-				average += Double.parseDouble(students[x][y]);
+				average += Double.parseDouble(students2[x][y]);
 				if(y == 3) {
 					average/=3;
 				}
 				if(average >= 7.0) {
-					students[x][5] = "Aprovado";
-					students[x][4] = String.valueOf(average);
+					students2[x][5] = "Aprovado";
+					students2[x][4] = String.valueOf(average);
 				}else {
-					students[x][5] = "Reprovado";
-					students[x][4] = String.valueOf(average);
+					students2[x][5] = "Reprovado";
+					students2[x][4] = String.valueOf(average);
 				}
 			}
 		}
